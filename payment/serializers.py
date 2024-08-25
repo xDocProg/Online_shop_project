7ff0d9ec6e0ff09034b1ac2b526e19fbf6e52dd4
Vlad Chirkov <vladchirkov777@gmail.com>
2024-08-25 16:55:56 +0500

    Add validate in payment

--- payment/serializers.py
+++ payment/serializers.py
@@ -8,3 +8,8 @@ class PaymentSerializer(serializers.ModelSerializer):
         fields = ['id', 'order', 'amount', 'status', 'payment_date']
         read_only_fields = ['amount', 'status']
 
+    def validate(self, data):
+        order = data.get('order')
+        if Payment.objects.filter(order=order).exists():
+            raise serializers.ValidationError("Платеж для этого заказа уже существует.")
+        return data
